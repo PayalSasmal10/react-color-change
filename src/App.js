d50af9767b5961435f8af078eaf7661a772f51e0
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-29 07:49:33 +0530

    react color change is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [colorCircle, setColorCircle] = useState("black");
+
+  const colorHandleChange = (colors) =>
+    colors === "green"
+      ? setColorCircle("green")
+      : colors === "blue"
+      ? setColorCircle("#1B6B93")
+      : colors === "red"
+      ? setColorCircle("red")
+      : colors === "yellow"
+      ? setColorCircle("yellow")
+      : colors === "black"
+      ? setColorCircle("black")
+      : "black";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="box">
+        <div className="shape" style={{ background: colorCircle }}></div>
+        <button
+          className="color black"
+          onClick={() => colorHandleChange("black")}
         >
-          Learn React
-        </a>
-      </header>
+          Black
+        </button>
+        <button
+          onClick={() => colorHandleChange("green")}
+          className="color green"
+        >
+          Green
+        </button>
+        <button
+          onClick={() => colorHandleChange("blue")}
+          className="color white"
+        >
+          Blue
+        </button>
+        <button onClick={() => colorHandleChange("red")} className="color red">
+          Red
+        </button>
+        <button
+          onClick={() => colorHandleChange("yellow")}
+          className="color yellow"
+        >
+          Yellow
+        </button>
+      </div>
     </div>
   );
 }
